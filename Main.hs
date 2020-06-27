107df808dda2fa3417f83a26c7c45c934ad33168
chocolatier <16237479+chocolatier@users.noreply.github.com>
2020-06-27 17:50:08 +0530

    dumping content from file

--- Main.hs
+++ Main.hs
@@ -1,9 +1,18 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Web.Scotty
+import Text.Pandoc
+import qualified Data.Text.Lazy as L
+import qualified Data.Text.Lazy.IO as L.IO
+import qualified Data.Text as T
+import Control.Monad.Trans (liftIO)
 
 main :: IO ()
 main = scotty 3000 $ do
-  get "/:word" $ do
-    beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+  get "/:page" $ do
+    page <- param "page"
+    content <- liftIO $ L.IO.readFile $ mconcat ["./test/", page, ".tex"] 
+    html $ content
+
+texToHTML :: PandocMonad m => T.Text -> m T.Text
+texToHTML str = readLaTeX def str >>= writeHtml5String def
